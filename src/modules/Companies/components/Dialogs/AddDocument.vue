40c351f3ed78aac41cb275a96841e917de48a54d
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-06-30 11:57:32 -0400

    New files

---
+++ src/modules/Companies/components/Dialogs/AddDocument.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+onMounted(() => {
+  console.log('mounted');
+});
+</script>
+
+<template>
+  <q-card>
+    <q-card-section class="row items-center">
+      <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+      <span class="q-ml-sm"
+        >You are currently not connected to any network.</span
+      >
+    </q-card-section>
+    <q-card-actions align="right">
+      <q-btn flat label="Cancel" color="primary" v-close-popup />
+      <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+    </q-card-actions>
+  </q-card>
+</template>
